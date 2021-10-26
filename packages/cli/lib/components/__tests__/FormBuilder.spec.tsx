9dbfda2f0088c9d0b6408459267436e4dee7ba5e
Leonel Vieyra <me@leonelv.com>
2021-10-26 17:03:04 -0300

    test(cli): fix mocks

--- packages/cli/lib/components/__tests__/FormBuilder.spec.tsx
+++ packages/cli/lib/components/__tests__/FormBuilder.spec.tsx
@@ -1,22 +1,26 @@
 import React from 'react'
+import { Text } from 'ink'
 import { render } from 'ink-testing-library'
 import FormBuilder from '../FormBuilder'
+import useNavegableForm from '../../hooks/useNavegableForm'
 import remoteConfig from '../../forms/remotes.form'
 
-jest.mock('../../hooks/useNavegableForm', () => ({
-  useNavegableForm: () => {
-    const state = {
-      name: 'app_title',
-      label: 'foo',
-      type: 'select',
-      index: 2
-    }
-    return [{ CurrentComponent: jest.fn(), state }, jest.fn()]
-  }
-}))
+const state = {
+  name: 'app_title',
+  label: 'foo',
+  type: 'select',
+  index: 2
+}
+
+jest.mock('../../hooks/useNavegableForm')
 
 describe('FormBuiler', () => {
   test('should return form builder component', () => {
+    // @ts-ignore
+    useNavegableForm.mockReturnValueOnce([
+      { CurrentComponent: jest.fn().mockReturnValue(<Text>asdad</Text>), state },
+      jest.fn()
+    ])
     const { stdout } = render(<FormBuilder form={remoteConfig as any} />)
     console.log(stdout.lastFrame())
   })
